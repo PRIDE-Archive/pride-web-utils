3796088c0aacfdfa6c1af55fcb4f266fa0fcee05
Rui Wang <harry.r.wang@gmail.com>
2013-11-21 14:00:26 +0000

    add SecureRestTemplateFactory to provide HTTP basic aware rest template

---
+++ src/main/java/uk/ac/ebi/pride/web/util/template/SecureRestTemplateFactory.java
@@ -0,0 +1,34 @@
+package uk.ac.ebi.pride.web.util.template;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Rui Wang
+ * @version $Id$
+ */
+public final class SecureRestTemplateFactory {
+
+    public static RestTemplate getTemplate(String userName, String password) {
+        // setup authentication
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        Credentials credentials = new UsernamePasswordCredentials(userName, password);
+        credentialsProvider.setCredentials(AuthScope.ANY, credentials);
+
+        // update user summary
+        DefaultHttpClient client = new DefaultHttpClient();
+        client.setCredentialsProvider(credentialsProvider);
+
+        HttpComponentsClientHttpRequestFactory contextAwareHttpRequestFactory = new PreEmptiveAuthHttpRequestFactory(client);
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(contextAwareHttpRequestFactory);
+        return restTemplate;
+    }
+}
