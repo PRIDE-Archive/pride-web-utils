a3dc2173982f2c7cfbd32644d990ed2e7fe7b14f
rwang <rwang@84e480dd-4284-0410-bcb6-dc83b1e7cf5b>
2014-05-08 12:14:17 +0000

    add resource not found and internal error exception

--- pride-web-utils/trunk/src/main/java/uk/ac/ebi/pride/web/util/exception/RestErrorRegistry.java
+++ pride-web-utils/trunk/src/main/java/uk/ac/ebi/pride/web/util/exception/RestErrorRegistry.java
@@ -4,6 +4,8 @@ import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.social.InternalServerErrorException;
+import org.springframework.social.ResourceNotFoundException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -30,7 +32,9 @@ public enum RestErrorRegistry {
     HTTP_MEDIA_TYPE_NOT_ACCEPTABLE(HttpMediaTypeNotAcceptableException.class, HttpStatus.NOT_ACCEPTABLE, 10005, "error.media.type.unacceptable", null),
     HTTP_MEDIA_TYPE_NOT_SUPPORTED(HttpMediaTypeNotSupportedException.class, HttpStatus.UNSUPPORTED_MEDIA_TYPE, 10006, "error.unsupported.media.type", null),
     ACCESS_DENY_EXCEPTION(AccessDeniedException.class, HttpStatus.UNAUTHORIZED, 10007, "error.access.denied", null),
-    METHOD_ARGUMENT_NOT_VALID_EXCEPTION(MethodArgumentNotValidException.class, HttpStatus.BAD_REQUEST, 10008, "error.request.argument.validation", null);
+    METHOD_ARGUMENT_NOT_VALID_EXCEPTION(MethodArgumentNotValidException.class, HttpStatus.BAD_REQUEST, 10008, "error.request.argument.validation", null),
+    RESOURCE_NOT_FOUND_EXCEPTION(ResourceNotFoundException.class, HttpStatus.NOT_FOUND, 10009, "error.resource.not.found", null),
+    INTERNAL_SERVER_ERROR(InternalServerErrorException.class, HttpStatus.INTERNAL_SERVER_ERROR, 10010, "error.internal.server.error", null);
 
     private Class<? extends Exception> exceptionClass;
     private HttpStatus status;
